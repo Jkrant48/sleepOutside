df56c7c600302b8cf10654e7734160b388c2f2fd
Jonathan Koranteng <kor22002@byui.edu>
2024-05-04 21:10:00 +0000

    Dynamic product page

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,16 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductDetails from "./productDetails.mjs"; //new
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  var cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+const productId = getParams("product");
+console.log(dataSource.findProductById(productId));
+
+//new
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
