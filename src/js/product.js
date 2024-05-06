daa5e0b56718722d60f3a340e26f3d29e131c32a
Jonathan Koranteng <kor22002@byui.edu>
2024-05-06 19:26:59 +0000

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ import ProductData from "./productData.mjs";
 const dataSource = new ProductData("tents");
 
 const productId = getParams("product");
-console.log(dataSource.findProductById(productId));
+//console.log(dataSource.findProductById(productId));
 
 //new
 const product = new ProductDetails(productId, dataSource);
